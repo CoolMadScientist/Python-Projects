295cfce8d2ca8979b7aebae2a8ef68bcfae9e9cb
CoolMadScientist <dlevin2004@gmail.com>
2022-08-09 23:39:30 +0300

    new calculator

--- Ext_Functions/calculator.py
+++ Ext_Functions/calculator.py
@@ -1,5 +1,5 @@
 from logo import logo 
-print(logo)
+
 
 # Operations
 def add(n1, n2):
@@ -21,38 +21,32 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
 
-
-for symbol in operations:
-    print(symbol)
-
-more_calculations = True
-
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))    
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-
-while more_calculations == True:
+def calculator():
+    print(logo)
     
-    if input(f"Type 'y' to coninue calculating with {first_answer} or type 'n' to exit: ") == "n":
-        more_calculations = False
+    num1 = float(input("What's the first number?: "))
+    
+    for symbol in operations:
+        print(symbol)
 
-    operation_symbol = input("Pick another operation: ")
-    num3 = int(input("What's the next number?: "))    
-    calculation_function = operations[operation_symbol]
-    second_answer = calculation_function(first_answer, num3)
+    more_calculations = True
+    while more_calculations:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the second number?: "))    
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        
 
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    print(f"{first_answer} {operation_symbol} {num3} = {second_answer}") 
+        if input(f"Type 'y' to coninue calculating with {answer} or type 'n' to :start a new calculation: ") == "y":
+            num1 = answer
+        else: 
+            more_calculations = False
+            calculator()
 
-    first_answer = second_answer
-
-
+calculator()
 
 
 
