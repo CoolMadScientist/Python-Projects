2ba1556bd08f7ea421c7efedf50420cc041e79bc
CoolMadScientist <dlevin2004@gmail.com>
2022-08-09 23:06:00 +0300

    functions

---
+++ Ext_Functions/calculator.py
@@ -0,0 +1,58 @@
+from logo import logo 
+print(logo)
+
+# Operations
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2): 
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+
+
+for symbol in operations:
+    print(symbol)
+
+more_calculations = True
+
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What's the second number?: "))    
+calculation_function = operations[operation_symbol]
+first_answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+
+while more_calculations == True:
+    
+    if input(f"Type 'y' to coninue calculating with {first_answer} or type 'n' to exit: ") == "n":
+        more_calculations = False
+
+    operation_symbol = input("Pick another operation: ")
+    num3 = int(input("What's the next number?: "))    
+    calculation_function = operations[operation_symbol]
+    second_answer = calculation_function(first_answer, num3)
+
+
+    print(f"{first_answer} {operation_symbol} {num3} = {second_answer}") 
+
+    first_answer = second_answer
+
+
+
+
+
